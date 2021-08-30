d33fbf7aa24bc5eb3a68eebf25a1abd0b4716608
Sushilkumar Shisode <sushilkumar.shisode@webklipper.com>
2021-08-30 15:27:48 +0530

    Refactored code to make deduping faster and concise

--- sublime_zk.py
+++ sublime_zk.py
@@ -579,9 +579,10 @@ class Autobib:
 
     @staticmethod
     def dedupe(seq):
-        seen = set()
-        seen_add = seen.add
-        return [x for x in seq if not (x in seen or seen_add(x))]
+        """
+        Deduplicates input sequence
+        """
+        return list(dict.fromkeys(seq))
 
     @staticmethod
     def find_citations(text, citekeys):
